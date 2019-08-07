539d5c6f9a34c3d6eb8402a6c57979a9eed213e7
Roberto Alfonso <robertoa@prx.com.ar>
2019-08-07 14:25:00 -0300

    Replaced IdentityUser for ApplicationUser

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTodo.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
