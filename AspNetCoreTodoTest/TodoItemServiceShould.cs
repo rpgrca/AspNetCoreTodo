27f2e18ab77db82e6e842a963a90156c1b5bfa56
Roberto <rpgrca@gmail.com>
2019-08-29 21:02:50 -0300

    Refactored

--- AspNetCoreTodoTest/TodoItemServiceShould.cs
+++ AspNetCoreTodoTest/TodoItemServiceShould.cs
@@ -10,28 +10,48 @@ namespace AspNetCoreTodo.Test
 {
     public class TodoItemServiceShould
     {
+        private const string _DefaultUserId = "fake-000";
+        private const string _DefaultUserName = "fake@example.com";
+        private const string _DefaultItemTitle = "Testing";
+
+        private static void ClearDataBase(DbContextOptions<ApplicationDbContext> options) {
+            using (var context = new ApplicationDbContext(options)) {
+                context.Database.EnsureDeleted();
+            }
+        }
+
+        private static TodoItem CreateTodoItem(DateTimeOffset dateTimeOffset, string userId, string itemTitle = _DefaultItemTitle, bool completed = false)
+        {
+            return new TodoItem
+            {
+                Title = itemTitle,
+                DueAt = dateTimeOffset,
+                UserId = userId,
+                IsDone = completed
+            };
+        }
+
+        private static ApplicationUser CreateFakeUser(string userId = _DefaultUserId, string userName = _DefaultUserName)
+        {
+            return new ApplicationUser
+            {
+                Id = userId,
+                UserName = userName,
+            };
+        }
+
         [Fact]
         public async Task AddNewItemAsIncompleteWithDueDate()
         {
             // Given
             var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
             var dateTimeOffset = DateTimeOffset.Now;
-            const string userId = "fake-000";
-            const string userName = "fake@example.com";
-            const string itemTitle = "Testing";
 
-            using (var context = new ApplicationDbContext(options)) {
+            using (var context = new ApplicationDbContext(options))
+            {
                 var service = new TodoItemService(context);
-
-                var fakeUser = new ApplicationUser {
-                    Id = userId,
-                    UserName = userName,
-                };
-
-                var todoItem = new TodoItem {
-                    Title = itemTitle,
-                    DueAt = dateTimeOffset
-                };
+                ApplicationUser fakeUser = CreateFakeUser();
+                TodoItem todoItem = CreateTodoItem(dateTimeOffset, userId:fakeUser.Id);
 
                 // When
                 await service.AddItemAsync(todoItem, fakeUser);
@@ -43,32 +63,25 @@ namespace AspNetCoreTodo.Test
                 Assert.Equal(1, itemsInDatabase);
 
                 var item = await context.Items.FirstAsync();
-                Assert.Equal(itemTitle, item.Title);
+                Assert.Equal(_DefaultItemTitle, item.Title);
                 Assert.False(item.IsDone);
                 Assert.Equal(dateTimeOffset, item.DueAt);
             }
+
+            ClearDataBase(options);
         }
 
-       [Fact]
+        [Fact]
         public async Task AddNewItemAsIncompleteWithoutDueDate()
         {
             // Given
             var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
-            const string userId = "fake-000";
-            const string userName = "fake@example.com";
-            const string itemTitle = "Testing";
+            var dateTimeOffset = DateTimeOffset.Now;
 
             using (var context = new ApplicationDbContext(options)) {
                 var service = new TodoItemService(context);
-
-                var fakeUser = new ApplicationUser {
-                    Id = userId,
-                    UserName = userName
-                };
-
-                var todoItem = new TodoItem {
-                    Title = itemTitle
-                };
+                var fakeUser = CreateFakeUser();
+                var todoItem = CreateTodoItem(dateTimeOffset, userId:fakeUser.Id);
 
                 // When
                 await service.AddItemAsync(todoItem, fakeUser);
@@ -80,30 +93,96 @@ namespace AspNetCoreTodo.Test
                 Assert.Equal(1, itemsInDatabase);
 
                 var item = await context.Items.FirstAsync();
-                Assert.Equal(itemTitle, item.Title);
+                Assert.Equal(_DefaultItemTitle, item.Title);
                 Assert.False(item.IsDone);
 
                 var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
                 Assert.True(difference < TimeSpan.FromSeconds(1));
             }
+
+            ClearDataBase(options);
         }
 
         [Fact]
-        // EJ. MarkDoneASync method returns false if its passed an ID that doesnt exist
-        public void MarkDoneAsyncMethodWithInexistantId() {
-            Assert.True(false, "Not implemented");
+        public async Task MarkDoneAsyncMethodWithInexistantId() {
+            // Given
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+            var dateTimeOffset = DateTimeOffset.Now;
+
+            using (var context = new ApplicationDbContext(options)) {
+                var service = new TodoItemService(context);
+                var fakeUser = CreateFakeUser();
+
+                // When
+                bool result = await service.MarkDoneAsync(Guid.NewGuid(), fakeUser);
+
+                // Then
+                Assert.False(result);
+            }
+
+            ClearDataBase(options);
         }
 
         [Fact]
-        // EJ. MarkDoneAsync method returns true when it makes a valid item as complete
-        public void MarkDoneAsyncMethodWithValidId() {
-            Assert.True(false, "Not implemented");
+        public async Task MarkDoneAsyncMethodWithValidId() {
+            // Given
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+            var dateTimeOffset = DateTimeOffset.Now;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+                var fakeUser = CreateFakeUser();
+                var todoItem = CreateTodoItem(dateTimeOffset, userId:fakeUser.Id);
+
+                await service.AddItemAsync(todoItem, fakeUser);
+            }
+
+            using (var context = new ApplicationDbContext(options)) {
+                var service = new TodoItemService(context);
+                var fakeUser = CreateFakeUser();
+                var itemsInDatabase = await context.Items.CountAsync();
+                var item = await context.Items.FirstAsync();
+
+                bool result = await service.MarkDoneAsync(item.Id, fakeUser);
+                Assert.True(result);
+            }
+
+            ClearDataBase(options);
         }
 
         [Fact]
-        // EJ. GetIncompleteItemsASync method returns only the items owned by a particular user
-        public void GetIncompleteItemsAsyncByUser() {
-            Assert.True(false, "Not implemented");
+        public async Task GetIncompleteItemsAsyncByUser() {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+            DateTime dueAt = DateTime.Today.AddDays(5);
+            ApplicationUser userWithIncompletedItems = CreateFakeUser();
+            ApplicationUser otherUser = CreateFakeUser("fake-001", "fake1@example.com");
+            TodoItem todoItemCompleted = CreateTodoItem(dueAt.AddDays(-10), userWithIncompletedItems.Id, "TodoItemCompleted", true);
+            TodoItem todoItemIncompleted = CreateTodoItem(dueAt, userWithIncompletedItems.Id, "TodoItemIncompleted");
+            TodoItem todoItemFromOtherUser = CreateTodoItem(dueAt.AddDays(5), otherUser.Id, "TodoItemFromOtherUser");
+
+            using (var context = new ApplicationDbContext(options)) {
+                await context.Items.AddAsync(todoItemCompleted);
+                await context.Items.AddAsync(todoItemIncompleted);
+                await context.Items.AddAsync(todoItemFromOtherUser);
+
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new ApplicationDbContext(options)) {
+                var service = new TodoItemService(context);
+                var todoItemsIncompletedForUser = await service.GetIncompleteItemsAsync(userWithIncompletedItems);
+                Assert.True(todoItemsIncompletedForUser.Length == 1);
+
+                var item = todoItemsIncompletedForUser[0];
+                Assert.Equal(todoItemIncompleted.Id, item.Id);
+                Assert.Equal(todoItemIncompleted.DueAt, item.DueAt);
+                Assert.False(item.IsDone);
+                Assert.Equal(todoItemIncompleted.Title, item.Title);
+                Assert.Equal(todoItemIncompleted.UserId, userWithIncompletedItems.Id);
+            }
+
+            ClearDataBase(options);
         }
     }
 }
