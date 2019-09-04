2c33ccafacc21086db88f495873caa628f39aaf7
Roberto <rpgrca@gmail.com>
2019-09-03 21:58:27 -0300

    Added post example

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -33,5 +33,13 @@ namespace TodoApi.Controllers {
 
             return todoItem;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item) {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id}, item);
+        }
     }
 }
