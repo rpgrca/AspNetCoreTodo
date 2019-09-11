e4ceaba88bc80a438085893ae19f3c74fd8bf13f
Roberto <rpgrca@gmail.com>
2019-09-10 21:43:01 -0300

    Add Put, Delete handlers

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -19,12 +19,7 @@ namespace TodoApi.Controllers {
             }
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems() {
-            return await _context.TodoItems.ToListAsync();
-        }
-
-        [HttpGet("id")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<TodoItem>> GetTodoItem(long id) {
             var todoItem = await _context.TodoItems.FindAsync(id);
             if (todoItem == null) {
@@ -34,12 +29,53 @@ namespace TodoApi.Controllers {
             return todoItem;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems() {
+            return await _context.TodoItems.ToListAsync();
+        }
+
         [HttpPost]
         public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item) {
             _context.TodoItems.Add(item);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TodoItem>> PutTodoItem(long id, TodoItem item) {
+            if (id != item.Id) {
+                return BadRequest();
+            }
+
+#if false
+            _context.Entry(item).State = EntityState.Modified;
+#else
+            var myItem = await _context.TodoItems.FindAsync(id);
+            if (myItem == null) {
+                return BadRequest();
+            }
+
+            myItem.Name = item.Name;
+            myItem.IsComplete = item.IsComplete;
+#endif
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id) {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null) {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
         }
     }
 }
