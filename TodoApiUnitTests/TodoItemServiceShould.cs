4fe9073c702e921d3eba98a503183d98fba6c073
Roberto <reybrujo@gmail.com>
2019-10-03 01:48:45 -0300

    Added unit test for TodoApi

---
+++ TodoApiUnitTests/TodoItemServiceShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TodoApiUnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
