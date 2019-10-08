5326958e78ec0f41fbe8546eed4ae86b06882724
Roberto <reybrujo@gmail.com>
2019-10-08 01:26:58 -0300

    Fixed

--- TodoApi/Models/ApplicationDBContext.cs
+++ TodoApi/Models/ApplicationDBContext.cs
@@ -3,10 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TodoApi.Models {
     public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+/*
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlite("DataSource=todoitems.db");
-        }
+        }*/
 
-        public DbSet<TodoItem> TodoItems {set; get;}
+        public DbSet<TodoItem> TodoItems { set; get; }
     }
 }
