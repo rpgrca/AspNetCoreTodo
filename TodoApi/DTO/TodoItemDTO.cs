df9fb54c5bdf212003aa506871b905d7e116d785
Roberto <reybrujo@gmail.com>
2019-10-08 01:26:27 -0300

    Added fields to DTO

--- TodoApi/DTO/TodoItemDTO.cs
+++ TodoApi/DTO/TodoItemDTO.cs
@@ -6,8 +6,15 @@ namespace TodoApi.DTO {
     [DebuggerDisplay("{Name}")]
     public class TodoItemDTO {
         [Required]
-        public string Name {get; set;}
+        public string Name { get; set; }
+
         [Required]
-        public bool IsComplete {get;set;}
+        public bool IsComplete { get; set; }
+
+        public long Order { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTimeOffset DueAt { get; set; }
     }
 }
