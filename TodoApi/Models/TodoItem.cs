a349bf38cb00c3d244c716fd591777fd391db595
Roberto <reybrujo@gmail.com>
2019-10-08 01:25:47 -0300

    Added some more fields to model

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -5,11 +5,20 @@ using System.ComponentModel.DataAnnotations;
 namespace TodoApi.Models {
     [DebuggerDisplay("{Name}")]
     public class TodoItem {
-        public long Id { get;set; }
+        public long Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         public bool IsComplete { get; set; }
+
+        public DateTimeOffset DueAt { get; set; }
+
+        public string Description { get; set; }
+
+        public long Order { get; set; }
+
         public User Responsible { get; set; }
     }
 }
